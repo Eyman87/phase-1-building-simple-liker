13ba14871f60f543dccfbf52973b19593b2dee43
Eyman Eghterafi <eghterafieyman@gmail.com>
2022-03-16 20:39:47 -0500

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,22 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function handleClick(e) {
+  const heart = e.target.innerText
+  mimicServerCall()
+  .then(() => {})
+  .catch(() => {
+    const element = document.getElementById("modal");
+  element.classList.remove('hidden');
+  setTimeout(document.body.hidden, 3000)
+})
+  
+  console.log(heart)
+}
+const hearts = document.querySelectorAll(".like-glyph");
+  for (const heart of hearts) {
+    heart.addEventListener("click", handleClick)
+  }
 
 
 
